e2bcc4c2791ee6ca48db750ff7a5b5eed508093e
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2024-08-06 13:51:16 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="text-3xl mt-4 text-blue-500 font-bold text-center">
-        React Forms Example
+        React Bootstrap Forms Example
       </h1>
       <p className="text-center mb-4 text-gray-600">
         Este es un formulario que emula el funcionamiento de Google Forms
